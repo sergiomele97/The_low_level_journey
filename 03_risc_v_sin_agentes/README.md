22d43f4ffa51ae49b46fba0c13cd0f20315335d1
Sergio Melero <sergiomele97@gmail.com>
2026-01-11 20:26:59 +0100

    escribir y leer

--- 03_risc_v_sin_agentes/README.md
+++ 03_risc_v_sin_agentes/README.md
@@ -4,3 +4,13 @@ Voy a evitar depender de ellos, entonces hay que definir nuevos objetivos realis
 
 Quiero partir del set de instrucciones de risc-v, probablemente empezar en 32 bits e intentar construir desde ahí. Acepto utilizar un ensamblador como nasm o el que toque, pero quiero no usar lenguajes de programación ni más abstracciones que no haya hecho yo.
 
+---
+para correr: make run
+para salir: ctrl + a => x
+---
+para debug: make debug (en una terminal)
+
+gdb-multiarch kernel.elf -ex "target remote :1234" -ex "layout asm" -ex "layout regs" -ex "break _start" -ex "continue" (en otra terminal y escribir "si" y pulsar enter)
+
+para salir: q => enter
+---
--- 03_risc_v_sin_agentes/kernel.s
+++ 03_risc_v_sin_agentes/kernel.s
@@ -2,18 +2,26 @@
 .globl _start
 
 _start:
-    li t0, 0x10000000      # Dirección de la UART
+    li t0, 0x10000000      # Dirección base UART
 
 bucle_eco:
-    # 1. Leer de la UART (Entrada)
-    # En la máquina virt, leer de 0x10000000 te da el carácter pulsado
-    lbu t1, 0(t0)          
-    
-    # 2. Comprobar si hay dato
-    # (A veces la UART devuelve 0 o -1 si no hay nada, 
-    # pero QEMU en este modo suele esperar a que pulses)
-    
-    # 3. Escribir en la UART (Salida)
-    sb t1, 0(t0)           
+    # --- 1. ESPERAR A QUE HAYA DATO ---
+esperar_dato:
+    lbu t1, 5(t0)          # Leemos el registro LSR (base + 5)
+    andi t1, t1, 1         # Máscara: nos quedamos solo con el Bit 0
+    beqz t1, esperar_dato  # Si es 0, vuelve a preguntar (no hay nada)
+
+    # --- 2. LEER EL DATO ---
+    lbu t2, 0(t0)          # Ahora sí, leemos la letra real
+
+    # --- 3. ESPERAR A QUE LA UART PUEDA TRANSMITIR ---
+    # El Bit 5 del LSR nos dice si el transmisor está vacío
+esperar_transmisor:
+    lbu t1, 5(t0)
+    andi t1, t1, 0x20      # 0x20 es el Bit 5 (32 en decimal)
+    beqz t1, esperar_transmisor # Si es 0, la UART está ocupada enviando
+
+    # --- 4. ESCRIBIR EL DATO ---
+    sb t2, 0(t0)           
     
     j bucle_eco            # Volver a empezar
